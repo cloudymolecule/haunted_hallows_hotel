ed9e45b5e812b7d05e21f22424277545495677a0
Cloudymolecule <baires82@gmail.com>
2021-01-20 20:49:17 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+haunted_hallows_hotel = Hotel.new
+
+
+
+# guests
+# nickname, full_name, age, gender, investigator, believer, tech, psychic, bio
+guest_list = [
+    ["guest1", "Luke Skywalker", 28, "Male", 3, 5, 2, 5, "Trying to balance the force."],
+    ["guest2", "Ganondorf", 76, "Male", 3, 2, 4, 2, "King of Hyrule."]
+    ["guest3", "Aerith Gainsborough", 19, "Female", 5, 3, 1, 3, "We can, we will."]
+    ["guest4", "Thomas A. Anderson", 36, "Male", 2, 3, 1, 4, "There's only one."]
+    ["guest5", "Elliot Alderson", 26, "Male", 1, 5, 5, 5, "You did good kiddo."]
+]
+
+guest_list.each do |nickname, full_name, age, gender, investigator, believer, tech, psychic, bio |
+    Guest.create(
+        nickname: nickname,
+        full_name: full_name,
+        age: age,
+        gender: gender,
+        investigator: investigator,
+        believer: believer,
+        tech: tech,
+        psychic: psychic,
+        bio: bio
+    )
+end
+
