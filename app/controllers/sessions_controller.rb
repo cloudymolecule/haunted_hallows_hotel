d8fbdb2982301f16b7e794fd0e986f30a5128cff
Cloudymolecule <baires82@gmail.com>
2021-01-22 12:16:47 -0500

    Fix typos

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     def create
         @guest = Guest.find_by(nickname: sessions_params[:nickname])
         if @guest && @guest.authenticate(sessions_params[:password])
-            sessions[:guest_id] = @guest.id
+            session[:guest_id] = @guest.id
             redirect_to guest_path(@guest)
         else
             redirect_to login_path
