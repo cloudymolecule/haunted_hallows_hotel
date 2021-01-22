232cc140338920e5f5945557eca41168b7d78c79
Cloudymolecule <baires82@gmail.com>
2021-01-21 20:49:47 -0500

    Add sessions/new.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
+
     def new
+        @guest = Guest.find(params[:id])
     end
 
     def create
@@ -7,4 +9,11 @@ class SessionsController < ApplicationController
 
     def destroy
     end
+
+    private
+
+    def sessions_params
+        params.require(:guest).permit(:nickname, :password)
+    end
+
 end
