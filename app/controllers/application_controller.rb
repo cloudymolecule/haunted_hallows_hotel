1290218c3e64657dc2bec7b90e2fe7bbed683b58
Cloudymolecule <baires82@gmail.com>
2021-01-22 11:10:32 -0500

    Add helper methods to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    helper_method :logged_in?
+
+    def current_user
+        Guest.find_by(Id: session[:guest_id])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
 end
